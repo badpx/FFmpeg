85367c98f88823ba9c2e227f23ed99ec4be87009
badpx <badpxx@gmail.com>
2025-06-30 20:09:01 +0800

    Add compile dependencies on the mp3lame library.

--- buildffmpeg.sh
+++ buildffmpeg.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "错误：请指定构建安装的目标目录" >&2  # 输出到标准错误
+    echo "如果要使用相对路径请以\$PWD开头" >&2  # --prefix只能接受绝对路径
+    exit 1  # 返回非零状态码表示错误
+fi
+
 # 一次性清掉旧配置
 make distclean 2>/dev/null || true
 
 # 最小化配置
 emconfigure ./configure \
-    --prefix=$PWD/build          \
+    --prefix=$1          \
     --target-os=none \
     --arch=x86_32 \
     --enable-cross-compile \
@@ -16,17 +24,19 @@ emconfigure ./configure \
     --enable-avcodec --enable-avformat \
     --enable-swresample              \
     --enable-protocol=file           \
-    --enable-demuxer=wav,mp3,aac     \
+    --enable-libmp3lame \
+    --enable-encoder=libmp3lame \
     --enable-muxer=wav,mp3,adts      \
+    --enable-demuxer=wav,mp3,aac     \
     --enable-decoder=aac,mp3,pcm_s16le \
     --enable-parser=aac,mp3          \
     --ar=emar \
     --ranlib=emranlib \
     --cc=emcc \
     --cxx=em++ \
-    --extra-cflags="-O2 -DNDEBUG" \
-    --extra-cxxflags="-O2 -DNDEBUG"
-
+    --extra-cxxflags="-O2 -DNDEBUG"     \
+    --extra-cflags="-I$1/include -O2 -DNDEBUG"  \
+    --extra-ldflags="-L$1/lib -lmp3lame"
 # 编译 & 安装
 emmake make -j$(sysctl -n hw.logicalcpu)
 emmake make install
--- buildwasm.sh
+++ buildwasm.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "错误：请指定依赖库的安装目录" >&2  # 输出到标准错误
+    exit 1  # 返回非零状态码表示错误
+fi
 
 emcc \
-    -I./build/include/ \
-    -L./build/lib/  \
+    -I$1/include \
+    -L$1/lib  \
     ffmpeg_api.c \
     -lavformat -lavcodec -lswresample -lavutil -lm \
     -Wl,--no-entry  \
     -Wl,--export=get_avcodec_version    \
+    -Wl,--export=get_version_str    \
     -s STANDALONE_WASM=1    \
     -s ASSERTIONS=1 \
     -O1 \
